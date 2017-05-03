8ae3aca334946b95c490256d8ab22c6533208155
gazbert <gareth.jon.lynch@gmail.com>
2017-05-03 23:37:23 +0100

    Grammar and typo fixes in javadoc

--- bxbot-strategies/src/main/java/com/gazbert/bxbot/strategies/ExampleScalpingStrategy.java
+++ bxbot-strategies/src/main/java/com/gazbert/bxbot/strategies/ExampleScalpingStrategy.java
@@ -51,54 +51,56 @@ import java.util.List;
  * </p>
  * <p>
  * It was originally written to trade on <a href="https://btc-e.com">BTC-e</a>, but should work for any exchange.
- * It initiates trades to buy the base currency (BTC in this example) using the counter currency (USD in this example),
- * and then sells BTC at a higher price to take profit from the spread. The algorithm expects you to have deposited
- * sufficient USD into your exchange wallet in order to buy BTC.
+ * The algorithm will start by buying the base currency (BTC in this example) using the counter currency (USD in this example),
+ * and then sell the base currency (BTC) at a higher price to take profit from the spread. The algorithm expects you to have
+ * deposited sufficient counter currency (USD) into your exchange wallet in order to buy the base currency (BTC).
  * </p>
  * <p>
- * Wen it starts up, it places an order at the current BID price and uses x amount of counter currency (USD) to 'buy'
+ * When it starts up, it places an order at the current BID price and uses x amount of counter currency (USD) to 'buy'
  * the base currency (BTC). The value of x comes from the sample {project-root}/config/strategies.xml
  * 'counter-currency-buy-order-amount' config-item, currently set to 20 USD. Make sure that the value you use for x is
- * large enough to be able to meet the minimum BTC order size on the exchange, e.g. Bitfinex min order size is 0.01 BTC
- * as of 3 May 2017. The algorithm then waits for the buy order to fill...
+ * large enough to be able to meet the minimum BTC order size for the exchange you are trading on,
+ * e.g. the Bitfinex min order size is 0.01 BTC as of 3 May 2017.
+ * The algorithm then waits for the buy order to fill...
  * </p>
  * <p>
- * Once the buy order fills, the it then waits until the ASK price is at least y % higher than the previous buy fill price.
+ * Once the buy order fills, it then waits until the ASK price is at least y % higher than the previous buy fill price.
  * The value of y comes from the sample {project-root}/config/strategies.xml 'minimum-percentage-gain' config-item,
  * currently set to 1%. Once the % gain has been achieved, the algorithm will place a sell order at the current ASK price.
  * It then waits for the sell order to fill... and the cycle repeats.
  * </p>
  * <p>
- * The algorithm does not factor in being outbid when placing buy orders, i.e. it does not cancel its current order
+ * The algorithm does not factor in being outbid when placing buy orders, i.e. it does not cancel the current order
  * and place a new order at a higher price; it simply holds until the current BID price falls again. Likewise, the
  * algorithm does not factor in being undercut when placing sell orders; it does not cancel the current order and place
  * a new order at a lower price.
  * </p>
  * <p>
- * Chances are you will either get a stuck 'buy' order if the market is going up, or a stuck 'sell' order if the market
+ * Chances are you will either get a stuck buy order if the market is going up, or a stuck sell order if the market
  * goes down. You could manually execute the trades on the exchange and restart the bot to get going again... but a
- * much better solution would be to modify this algorithm to deal with it: cancel your current 'buy' order and place a
- * new order matching the current BID price, or cancel your current 'sell' order and place a new order matching the
+ * much better solution would be to modify this code to deal with it: cancel your current buy order and place a
+ * new order matching the current BID price, or cancel your current sell order and place a new order matching the
  * current ASK price. The {@link TradingApi} allows you to add this behaviour.
  * </p>
  * <p>
  * Remember to include the correct exchange fees (both buy and sell) in your buy/sell calculations when you write
- * your own algorithms, otherwise you'll end up bleeding fiat/crypto to the exchange... This simple algorithm relies on
- * {project-root}/config/strategies.xml 'minimum-percentage-gain' config-item value being high enough to make a profit
- * and cover the exchange fees. You could tweak the algo to call the
+ * your own algorithm. Otherwise, you'll end up bleeding fiat/crypto to the exchange...
+ * </p>
+ * <p>This demo algorithm relies on the {project-root}/config/strategies.xml 'minimum-percentage-gain' config-item value
+ * being high enough to make a profit and cover the exchange fees. You could tweak the algo to call the
  * {@link com.gazbert.bxbot.trading.api.TradingApi#getPercentageOfBuyOrderTakenForExchangeFee(String)} and
  * {@link com.gazbert.bxbot.trading.api.TradingApi#getPercentageOfSellOrderTakenForExchangeFee(String)} when calculating
- * the order to send to the exchange... See the sample {project-root}/config/samples/{exchange}/exchange.xml for an
- * idea of the different exchange fees.
- * </p>
- * <p>
- * This algorithm only manages 1 order at a time to keep things simple.
+ * the order to send to the exchange... See the sample {project-root}/config/samples/{exchange}/exchange.xml files for
+ * info on the different exchange fees.
  * </p>
  * <p>
  * The algorithm relies on config from the sample {project-root}/config/strategies.xml and
  * {project-root}/config/markets.xml files. You can pass additional config-items to your Strategy using the
  * {project-root}/config/strategies.xml file - you access it from the {@link #init(TradingApi, Market, StrategyConfig)}
  * method via the StrategyConfigImpl argument.
+ * </p>
+ * <p>
+ * This simple demo algorithm only manages 1 order at a time to keep things simple.
  * </p>
  * <p>
  * The Trading Engine will only send 1 thread through your strategy code at a time - you do not have to code for concurrency.
