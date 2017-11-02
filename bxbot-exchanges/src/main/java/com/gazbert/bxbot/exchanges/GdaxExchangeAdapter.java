78156aa18be87c5bcf023b4f60554c72a39193fb
gazbert <gareth.jon.lynch@gmail.com>
2017-11-02 00:22:44 +0000

    Patch for GDAX cancel order response no longer returning cancelled order id
    Been getting NPEs in GDAX adapter when receiving cancel order success
    responses as of 2 Nov 2017 (ish?).
    
    GDAX no longer includes orderId in an array type in cancel order response.
    
    Looks like cancel order response for single order now returns 200 OK
    and null payload on success:
    
    164607 [main] 2017-11-02 00:15:40,726 DEBUG GdaxExchangeAdapter cancelOrder() - Cancel Order response: ExchangeHttpResponse{statusCode=200, reasonPhrase=OK, payload=[null]}
    1
    
    Needs verifying...

--- bxbot-exchanges/src/main/java/com/gazbert/bxbot/exchanges/GdaxExchangeAdapter.java
+++ bxbot-exchanges/src/main/java/com/gazbert/bxbot/exchanges/GdaxExchangeAdapter.java
@@ -275,16 +275,20 @@ public final class GdaxExchangeAdapter extends AbstractExchangeAdapter implement
             LOG.debug(() -> "Cancel Order response: " + response);
 
             if (response.getStatusCode() == HttpURLConnection.HTTP_OK) {
-                // response payload is now a JSON array with 1 String entry: the orderId :-)
-                final String[] cancelledOrderId = gson.fromJson(response.getPayload(), String[].class);
-                if (cancelledOrderId[0].equals(orderId)) {
-                    return true;
-                } else {
-                    final String errorMsg = "Failed to cancel order on exchange due to Order Id mismatch. " +
-                            "OrderId sent: " + orderId + " ResponseOrderId: " + cancelledOrderId[0] + " Response: " + response;
-                    LOG.error(errorMsg);
-                    return false;
-                }
+
+                // TODO - gazbert PATCH - 1 Nov 2017 - looks like GDAX API response change - no longer returns cancelled orderId in array payload; it returns [null]... needs verifying/x-checking with GDAX REST API docs...
+                return true;
+
+//                // response payload is now a JSON array with 1 String entry: the orderId :-)
+//                final String[] cancelledOrderId = gson.fromJson(response.getPayload(), String[].class);
+//                if (cancelledOrderId[0].equals(orderId)) {
+//                    return true;
+//                } else {
+//                    final String errorMsg = "Failed to cancel order on exchange due to Order Id mismatch. " +
+//                            "OrderId sent: " + orderId + " ResponseOrderId: " + cancelledOrderId[0] + " Response: " + response;
+//                    LOG.error(errorMsg);
+//                    return false;
+//                }
             } else {
                 final String errorMsg = "Failed to cancel order on exchange. Details: " + response;
                 LOG.error(errorMsg);
