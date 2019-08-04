3cf7ae6deb208c1b3bb778ab6e894d268320eed8
gazbert <gazbert@users.noreply.github.com>
2019-08-04 20:38:31 +0100

    Merge Engine cleanup into master (#117)
    Merged.

--- bxbot-core/src/test/java/com/gazbert/bxbot/core/config/strategy/TestStrategyConfigItems.java
+++ bxbot-core/src/test/java/com/gazbert/bxbot/core/config/strategy/TestStrategyConfigItems.java
@@ -28,7 +28,7 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 /**
- * Test the StrategyConfigItems behaves as expected.
+ * Tests the StrategyConfigItems behaves as expected.
  *
  * @author gazbert
  */
