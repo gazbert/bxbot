eb3c097389f74d17e08e13a7fd12e6e031754f68
gazbert <gazbert@users.noreply.github.com>
2019-08-04 15:36:44 +0100

    #112 : Yet more Sonar test coverage boosting!

---
+++ bxbot-core/src/test/java/com/gazbert/bxbot/core/config/strategy/TestTradingStrategiesBuilder.java
@@ -0,0 +1,96 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2019 gazbert
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package com.gazbert.bxbot.core.config.strategy;
+
+import com.gazbert.bxbot.domain.market.MarketConfig;
+import com.gazbert.bxbot.domain.strategy.StrategyConfig;
+import com.gazbert.bxbot.exchange.api.ExchangeAdapter;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import org.easymock.EasyMock;
+import org.junit.Test;
+
+/**
+ * Tests the Trading Strategies Builder works as expected.
+ *
+ * @author gazbert
+ */
+public class TestTradingStrategiesBuilder {
+
+  private static final String STRATEGY_ID = "MyMacdStrategy_v3";
+  private static final String STRATEGY_NAME = "MACD Shorting algo";
+  private static final String STRATEGY_DESCRIPTION = "MACD Shorting algo description";
+  private static final String STRATEGY_CLASSNAME =
+      "com.gazbert.bxbot.core.config.strategy.strategies.TradingStrategyForClassnameInstantiation";
+  private static final String STRATEGY_BEAN = null;
+  private static final String STRATEGY_CONFIG_ITEM_NAME = "btc-sell-order-amount";
+  private static final String STRATEGY_CONFIG_ITEM_VALUE = "0.2";
+
+  private static final String MARKET_NAME = "BTC/USD";
+  private static final String MARKET_ID = "btc_usd";
+  private static final String MARKET_BASE_CURRENCY = "BTC";
+  private static final String MARKET_COUNTER_CURRENCY = "USD";
+  private static final boolean MARKET_IS_ENABLED = true;
+
+  @Test
+  public void testBuildingStrategies() {
+    final ExchangeAdapter exchangeAdapter = EasyMock.createMock(ExchangeAdapter.class);
+    TradingStrategiesBuilder.buildStrategies(
+        someStrategiesConfig(), someMarketsConfig(), exchangeAdapter);
+  }
+
+  private static List<StrategyConfig> someStrategiesConfig() {
+    final Map<String, String> configItems = new HashMap<>();
+    configItems.put(STRATEGY_CONFIG_ITEM_NAME, STRATEGY_CONFIG_ITEM_VALUE);
+
+    final StrategyConfig strategyConfig1 =
+        new StrategyConfig(
+            STRATEGY_ID,
+            STRATEGY_NAME,
+            STRATEGY_DESCRIPTION,
+            STRATEGY_CLASSNAME,
+            STRATEGY_BEAN,
+            configItems);
+
+    final List<StrategyConfig> allStrategies = new ArrayList<>();
+    allStrategies.add(strategyConfig1);
+    return allStrategies;
+  }
+
+  private static List<MarketConfig> someMarketsConfig() {
+    final MarketConfig marketConfig1 =
+        new MarketConfig(
+            MARKET_ID,
+            MARKET_NAME,
+            MARKET_BASE_CURRENCY,
+            MARKET_COUNTER_CURRENCY,
+            MARKET_IS_ENABLED,
+            STRATEGY_ID);
+    final List<MarketConfig> allMarkets = new ArrayList<>();
+    allMarkets.add(marketConfig1);
+    return allMarkets;
+  }
+}
