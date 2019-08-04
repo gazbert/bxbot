ce8b069c76be856199716abc13c581ee71ac5822
gazbert <gazbert@users.noreply.github.com>
2019-08-04 21:16:47 +0100

    Revert "Merge Engine cleanup into master (#117)"
    This reverts commit 3cf7ae6d
    
    Need to fix Spring App Context injection in TradingStrategiesBuilder

--- bxbot-core/src/test/java/com/gazbert/bxbot/core/config/strategy/TestStrategyConfigItems.java
+++ bxbot-core/src/test/java/com/gazbert/bxbot/core/config/strategy/TestStrategyConfigItems.java
@@ -28,7 +28,7 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 /**
- * Tests the StrategyConfigItems behaves as expected.
+ * Test the StrategyConfigItems behaves as expected.
  *
  * @author gazbert
  */
