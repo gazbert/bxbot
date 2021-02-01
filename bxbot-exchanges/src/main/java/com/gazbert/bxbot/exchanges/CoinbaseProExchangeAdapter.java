8fd79b6e4a5804b424f7e2a26a593345d81c0c73
gazbert <gazbert@users.noreply.github.com>
2021-02-01 23:19:13 +0000

    #132 : Add author to MIT

--- bxbot-exchanges/src/main/java/com/gazbert/bxbot/exchanges/CoinbaseProExchangeAdapter.java
+++ bxbot-exchanges/src/main/java/com/gazbert/bxbot/exchanges/CoinbaseProExchangeAdapter.java
@@ -2,6 +2,7 @@
  * The MIT License (MIT)
  *
  * Copyright (c) 2015 Gareth Jon Lynch
+ * Copyright (c) 2019 David Huertas
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of
  * this software and associated documentation files (the "Software"), to deal in
