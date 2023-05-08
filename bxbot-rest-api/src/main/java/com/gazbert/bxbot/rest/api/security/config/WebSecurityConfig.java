60f8b733fe66d83c0547c143931b938879a4c4c4
gazbert <gazbert@users.noreply.github.com>
2023-05-08 19:07:05 +0100

    #154 : Fix sonar whine

--- bxbot-rest-api/src/main/java/com/gazbert/bxbot/rest/api/security/config/WebSecurityConfig.java
+++ bxbot-rest-api/src/main/java/com/gazbert/bxbot/rest/api/security/config/WebSecurityConfig.java
@@ -33,7 +33,6 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -55,7 +54,6 @@ import org.springframework.web.filter.CorsFilter;
  */
 @Configuration
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig {
 
   private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
