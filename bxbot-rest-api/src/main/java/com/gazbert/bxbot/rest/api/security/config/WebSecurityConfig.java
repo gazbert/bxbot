a5128d0091bef2ef8d668268cef732af67477d6e
gazbert <gazbert@users.noreply.github.com>
2023-05-08 19:14:23 +0100

    #154 : Revert last change

--- bxbot-rest-api/src/main/java/com/gazbert/bxbot/rest/api/security/config/WebSecurityConfig.java
+++ bxbot-rest-api/src/main/java/com/gazbert/bxbot/rest/api/security/config/WebSecurityConfig.java
@@ -35,6 +35,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -54,6 +55,7 @@ import org.springframework.web.filter.CorsFilter;
  */
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig {
 
   private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
