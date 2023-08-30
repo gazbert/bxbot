927726ff964d5ec526e7ba3a92ce6d1a6da67a26
gazbert <gazbert@users.noreply.github.com>
2023-08-30 16:50:55 +0100

    #162 : Replaced deprecated @EnableGlobalMethodSecurity with @EnableMethodSecurity

--- bxbot-rest-api/src/main/java/com/gazbert/bxbot/rest/api/security/config/WebSecurityConfig.java
+++ bxbot-rest-api/src/main/java/com/gazbert/bxbot/rest/api/security/config/WebSecurityConfig.java
@@ -34,6 +34,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -55,7 +56,7 @@ import org.springframework.web.filter.CorsFilter;
  */
 @Configuration
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true)
+@EnableMethodSecurity
 public class WebSecurityConfig {
 
   private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
