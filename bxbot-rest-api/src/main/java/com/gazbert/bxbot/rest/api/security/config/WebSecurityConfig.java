9a2f82a1417179fa948688cce0fa22c2c5611142
gazbert <gazbert@users.noreply.github.com>
2023-08-30 17:34:07 +0100

    #162 : Removed banner from test output

--- bxbot-rest-api/src/main/java/com/gazbert/bxbot/rest/api/security/config/WebSecurityConfig.java
+++ bxbot-rest-api/src/main/java/com/gazbert/bxbot/rest/api/security/config/WebSecurityConfig.java
@@ -33,7 +33,6 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
